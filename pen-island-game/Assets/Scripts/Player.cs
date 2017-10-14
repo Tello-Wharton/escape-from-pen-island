050f7793906483700c513387d128131f03c75a03
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-14 21:34:55 +0200

    integrated bullet for shooting

--- pen-island-game/Assets/Scripts/Player.cs
+++ pen-island-game/Assets/Scripts/Player.cs
@@ -10,10 +10,14 @@ public class Player : MonoBehaviour {
 	private Transform[] spawnPoints;
 	private bool lastRespawnToggle = false;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start () {
 		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,9 +27,30 @@ public class Player : MonoBehaviour {
 		} else {
 			lastRespawnToggle = reSpawn;
 		}
-	}
 
-	private void Respawn() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    private void Respawn() {
 		int i = Random.Range (1, spawnPoints.Length);
 		transform.position = spawnPoints [i].transform.position;
 	}
