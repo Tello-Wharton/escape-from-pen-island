d08b88791765c19c8aedd442c59563d356d92cbe
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-14 18:03:55 +0200

    Made pen speed imrovement

--- pen-island-game/Assets/Pen1Script.cs
+++ pen-island-game/Assets/Pen1Script.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pen1Script : MonoBehaviour {
 
     Animator animator;
-    float speed = 4f;
+    float speed = 12f;
 
     // Use this for initialization
     void Start () {
