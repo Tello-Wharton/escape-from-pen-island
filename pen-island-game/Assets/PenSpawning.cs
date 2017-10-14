bf389e277d36b9c00b40a6294ee5863709841175
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-15 01:42:32 +0200

    Added pen spawning

---
+++ pen-island-game/Assets/PenSpawning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenSpawning : MonoBehaviour {
+
+    float count;
+
+
+    public GameObject penPrefab;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        count += Time.deltaTime;
+
+        if (count > 1){
+
+            count = 0;
+
+            var pen = (GameObject)Instantiate(
+            penPrefab,
+            transform.position,
+            transform.rotation);
+        }
+		
+	}
+}
---
+++ pen-island-game/Assets/Scripts/biscuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class biscuit : MonoBehaviour {
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
+    }
+}
