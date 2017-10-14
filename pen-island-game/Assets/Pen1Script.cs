aeac20ec65fa4d0a60975af056eca595fabfaec6
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-14 16:11:30 +0200

    Edited pen to agressively follow player

---
+++ pen-island-game/Assets/Pen1Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pen1Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var player = GameObject.Find("Player");
+
+        var heading = player.transform.position - transform.position;
+
+        var direction = heading / heading.magnitude;
+
+        transform.Translate(direction);
+
+		
+	}
+}
