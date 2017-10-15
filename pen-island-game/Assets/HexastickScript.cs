07d4afb7be566a191e3e573a8b9ddf5dc4b6b1b5
Isabelle Barrett <isabelle.barrett@kcl.ac.uk>
2017-10-15 04:04:36 +0200

    hexastick fix

---
+++ pen-island-game/Assets/HexastickScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HexastickScript : MonoBehaviour {
+
+	void onTriggerEnter (Collider collider) {
+		if (collider.gameObject.tag == "Player")
+			SceneManager.LoadScene ("End Screen", LoadSceneMode.Single);
+			
+	}
+	
+}
